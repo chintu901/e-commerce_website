768c6586bb8cf9841617e851a2dfcdea96c7822a
chintu901 <chetanranga92@gmail.com>
2025-03-12 21:42:05 +0530

    create javascript for the registration button

---
+++ static/js/registration_page.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("register_button").addEventListener("click", function(event) {
+        event.preventDefault();
+        window.location.href = "registration_page.html";
+    });
+});
