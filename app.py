377d75ee91213990cb1707f8e921f34fe3b7dff4
chintu901 <chetanranga92@gmail.com>
2025-03-13 22:15:37 +0530

    update the code

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('login_page.html')  # Ensure this file exists in `templates/`
+    return render_template('login_page.html')  
 
 @app.route('/registration_page')
 def registration_page():
-    return render_template('registration_page.html')  # Ensure this file exists in `templates/`
+    return render_template('registration_page.html')  
+
+@app.route('/forgot_password_page')
+def forgot_password_page():
+    return render_template('forgot_password_page.html')  
 
 if __name__ == '__main__':
     app.run(debug=True)
