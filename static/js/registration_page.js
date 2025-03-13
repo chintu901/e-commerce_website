0f396c5cedfeb6c06acb9129d2b5968e5c429bfc
chintu901 <chetanranga92@gmail.com>
2025-03-13 19:48:24 +0530

    fix the button width

--- static/js/registration_page.js
+++ static/js/registration_page.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("register_button").addEventListener("click", function(event) {
+    document.getElementById("register_new_account_button").addEventListener("click", function(event) {
         event.preventDefault();
         window.location.href = "/registration_page";
     });
