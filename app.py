acbc8bf89976a3c54d6e0633d3e46d34a1d8e3d0
chintu901 <chetanranga92@gmail.com>
2025-03-16 18:27:59 +0530

    add css for the main page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('login_page.html')  
+    return render_template('index.html')  
 
 @app.route('/registration_page')
 def registration_page():
