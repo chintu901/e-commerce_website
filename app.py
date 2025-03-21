8b36493be5422e51702096a8d0cd50cdf21f6403
chintu901 <chetanranga92@gmail.com>
2025-03-21 23:35:04 +0530

    added about us page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  
+    return render_template('about_page.html')  
 
 @app.route('/registration_page')
 def registration_page():
