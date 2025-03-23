3ae028e655e76d0cbb86a92f6d7754ce51fdd245
chintu901 <chetanranga92@gmail.com>
2025-03-23 12:42:46 +0530

    complete the home decor page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('shop_beds.html')  
+    return render_template('home_decor.html')  
 
 @app.route('/registration_page')
 def registration_page():
