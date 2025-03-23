482ef5a4a0edb02cc1f88803eba5c62ac0c5d440
chintu901 <chetanranga92@gmail.com>
2025-03-23 15:51:19 +0530

    complete sofas page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('dining_set.html')  
+    return render_template('all.html')  
 
 @app.route('/registration_page')
 def registration_page():
